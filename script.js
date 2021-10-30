573cf199332f4748bd0d83be45a3858b92ecacf3
NY4TI <tanim.iqbal13@gmail.com>
2021-10-30 01:04:20 -0400

    Url update, fetch request, button, event listener

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var repoList = document.querySelector('ul');
 var fetchButton = document.getElementById('searchBtn');
+var todoForm = document.getElementById('todo-form')
+var todoInput = document.getElementById('todo-text')
 var tempEl = document.getElementById('temp');
 var windEl = document.getElementById('wind');
 var humidEl = document.getElementById('humidity');
@@ -16,14 +18,13 @@ var tempEl4 = document.getElementById('temp4');
 var windEl4 = document.getElementById('wind4');
 var humidEl4 = document.getElementById('humidity4');
 var key = '90a759efbdc05d8004d62a39a8c185c9'
-var city = 'Atlanta'
-var lat = 33.7490
-var long = 84.3880
+// var city = 'Atlanta'
+// var lat = 33.7490
+// var long = 84.3880
 
+function getApi(citySearch) {
 
-function getApi() {
-  // replace `octocat` with anyone else's GitHub username
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+long+'&exclude=minutely&appid='+key;
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+citySearch+'&appid=90a759efbdc05d8004d62a39a8c185c9'
   console.log (requestUrl)
 
   fetch(requestUrl)
@@ -31,44 +32,60 @@ function getApi() {
       return response.json();
     })
     .then(function (data) { 
-        console.log (data.daily)
+      var lat = data.city.coord.lat
+      console.log (lat)
+      var lon = data.city.coord.lon
+      var mainUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=minutely&appid='+key;
+      console.log (lon)
+//---------------------------------------------------------------------------
+  fetch(mainUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (forcastData) { 
+
+        console.log (forcastData)
         //This is the forecast info for the 5 days
-        for (let i = 0; i < 4; i++) {
+            for (let i = 0; i < 4; i++) {
 
-        tempEl.textContent = "Temperature is "+data.daily[i].temp.day;
-        windEl.textContent = "Wind speed is "+data.daily[i].wind_speed;
-        humidEl.textContent = "Humidity is "+data.daily[i].humidity;
+            tempEl.textContent = "Temperature is "+forcastData.daily[i].temp.day;
+            windEl.textContent = "Wind speed is "+forcastData.daily[i].wind_speed;
+            humidEl.textContent = "Humidity is "+forcastData.daily[i].humidity;
 
-        tempEl1.textContent = "Temperature is "+data.daily[1].temp.day;
-        windEl1.textContent = "Wind speed is "+data.daily[1].wind_speed;
-        humidEl1.textContent = "Humidity is "+data.daily[1].humidity;
-        
-        tempEl2.textContent = "Temperature is "+data.daily[2].temp.day;
-        windEl2.textContent = "Wind speed is "+data.daily[2].wind_speed;
-        humidEl2.textContent = "Humidity is "+data.daily[2].humidity;
-        
-        tempEl3.textContent = "Temperature is "+data.daily[3].temp.day;
-        windEl3.textContent = "Wind speed is "+data.daily[3].wind_speed;
-        humidEl3.textContent = "Humidity is "+data.daily[3].humidity;
+            tempEl1.textContent = "Temperature is "+forcastData.daily[1].temp.day;
+            windEl1.textContent = "Wind speed is "+forcastData.daily[1].wind_speed;
+            humidEl1.textContent = "Humidity is "+forcastData.daily[1].humidity;
+            
+            tempEl2.textContent = "Temperature is "+forcastData.daily[2].temp.day;
+            windEl2.textContent = "Wind speed is "+forcastData.daily[2].wind_speed;
+            humidEl2.textContent = "Humidity is "+forcastData.daily[2].humidity;
+            
+            tempEl3.textContent = "Temperature is "+forcastData.daily[3].temp.day;
+            windEl3.textContent = "Wind speed is "+forcastData.daily[3].wind_speed;
+            humidEl3.textContent = "Humidity is "+forcastData.daily[3].humidity;
 
-        tempEl4.textContent = "Temperature is "+data.daily[4].temp.day;
-        windEl4.textContent = "Wind speed is "+data.daily[4].wind_speed;
-        humidEl4.textContent = "Humidity is "+data.daily[4].humidity;
-        }
-    //   for (var i = 0; i < 4; i++) {
-    //     var listItem = document.createElement('li');
-    //     listItem.textContent = data[i].html_url;
-    //     repoList.appendChild(listItem);
-    //   }
+            tempEl4.textContent = "Temperature is "+forcastData.daily[4].temp.day;
+            windEl4.textContent = "Wind speed is "+forcastData.daily[4].wind_speed;
+            humidEl4.textContent = "Humidity is "+forcastData.daily[4].humidity;
+            }
+      })
     });
 }
+// Add submit event to form
+todoForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var citySearch = todoInput.value.trim();
+  // Return from function early if submitted todoText is blank
+  if (citySearch === "") {
+    return;
+  }
+  // Add new todoText to todos array, clear the input
+  //todos.push(todoText);
+  todoInput.value = "";
+  // Store updated todos in localStorage, re-render the list
+  //storeTodos();
+  //renderTodos();
+  getApi(citySearch)
+});
 
-fetchButton.addEventListener('click', getApi);
 
-// var todo = document.getElementById("cities");
-// todo.addEventListener("click", function(event, cit) {
-//   var element = event.target;
-//    cit = element.textContent;
-
-//   getApi(cit); //call the API send the city
-// });
