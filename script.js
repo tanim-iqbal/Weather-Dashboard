9b33add2f6259ab0a6f02c2bda23cab1e95437ac
Tanim Iqbal <89047977+tanim-iqbal@users.noreply.github.com>
2022-02-09 01:43:56 -0500

    updated

--- script.js
+++ script.js
@@ -1,88 +1,110 @@
-var repoList = document.querySelector('ul');
-var fetchButton = document.getElementById('searchBtn');
-var todoForm = document.getElementById('todo-form')
-var todoInput = document.getElementById('todo-text')
-var tempEl = document.getElementById('temp');
-var windEl = document.getElementById('wind');
-var humidEl = document.getElementById('humidity');
-var tempEl1 = document.getElementById('temp1');
-var windEl1 = document.getElementById('wind1');
-var humidEl1 = document.getElementById('humidity1');
-var tempEl2 = document.getElementById('temp2');
-var windEl2 = document.getElementById('wind2');
-var humidEl2 = document.getElementById('humidity2');
-var tempEl3 = document.getElementById('temp3');
-var windEl3 = document.getElementById('wind3');
-var humidEl3 = document.getElementById('humidity3');
-var tempEl4 = document.getElementById('temp4');
-var windEl4 = document.getElementById('wind4');
-var humidEl4 = document.getElementById('humidity4');
-var key = '90a759efbdc05d8004d62a39a8c185c9'
-// var city = 'Atlanta'
-// var lat = 33.7490
-// var long = 84.3880
+const timeEl = document.getElementById("time");
+const dateEl = document.getElementById("date");
+const currentWeatherItemsEl = document.getElementById("current-weather-items");
+const timezone = document.getElementById("time-zone");
+const cityEl = document.getElementById("city");
+const weatherForecastEl = document.getElementById("weather-forecast");
+const currentTempEl = document.getElementById("current-temp");
 
-function getApi(citySearch) {
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thirsday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "April",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+citySearch+'&appid=90a759efbdc05d8004d62a39a8c185c9&units=imperial&'
-  console.log (requestUrl)
+const API_Key = "90a759efbdc05d8004d62a39a8c185c9";
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) { 
-      var lat = data.city.coord.lat
-      console.log (lat)
-      var lon = data.city.coord.lon
-      var mainUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=minutely&appid='+key+'&units=imperial&';
-      console.log (lon)
-//---------------------------------------------------------------------------
-  fetch(mainUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (forcastData) { 
-        
+setInterval(() => {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour;
+  const minutes = time.getMinutes();
+  const ampm = hour >= 12 ? "PM" : "AM";
 
-        console.log (forcastData)
-        //This is the forecast info for the 5 days
-            for (let i = 0; i < 4; i++) {
+  timeEl.innerHTML =
+    (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ":" + (minutes < 10? '0'+minutes: minutes) + " " + `<span id="am-pm">${ampm}</span>`;
 
-            tempEl.textContent = "Temperature is "+forcastData.daily[i].temp.day;
-            windEl.textContent = "Wind speed is "+forcastData.daily[i].wind_speed;
-            humidEl.textContent = "Humidity is "+forcastData.daily[i].humidity;
+  dateEl.innerHTML = days[day] + ", " + date + " " + months[month];
+}, 1000);
 
-            tempEl1.textContent = "Temperature is "+forcastData.daily[1].temp.day;
-            windEl1.textContent = "Wind speed is "+forcastData.daily[1].wind_speed;
-            humidEl1.textContent = "Humidity is "+forcastData.daily[1].humidity;
-            
-            tempEl2.textContent = "Temperature is "+forcastData.daily[2].temp.day;
-            windEl2.textContent = "Wind speed is "+forcastData.daily[2].wind_speed;
-            humidEl2.textContent = "Humidity is "+forcastData.daily[2].humidity;
-            
-            tempEl3.textContent = "Temperature is "+forcastData.daily[3].temp.day;
-            windEl3.textContent = "Wind speed is "+forcastData.daily[3].wind_speed;
-            humidEl3.textContent = "Humidity is "+forcastData.daily[3].humidity;
+getWeatherData();
+function getWeatherData() {
+  navigator.geolocation.getCurrentPosition((success) => {
+    console.log(success);
 
-            tempEl4.textContent = "Temperature is "+forcastData.daily[4].temp.day;
-            windEl4.textContent = "Wind speed is "+forcastData.daily[4].wind_speed;
-            humidEl4.textContent = "Humidity is "+forcastData.daily[4].humidity;
-            }
-      })
-    });
+    let { latitude, longitude } = success.coords;
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_Key}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        showWeatherData(data);
+      });
+  });
 }
-// Add submit event to form
-todoForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-  var citySearch = todoInput.value.trim();
-  // Return from function early if submitted todoText is blank
-  if (citySearch === "") {
-    return;
-  }
-  getApi(citySearch)
-});
 
-//Convertion from kelvin to fahrenheit
-//var kel2far = ((forcastData.daily[i].temp.day-273.15)*1.8)+32
-// console.log (kel2far)
+function showWeatherData(data) {
+  let { humidity, wind_speed } = data.current;
+
+  timezone.innerHTML = data.timezone;
+  cityEl.innerHTML = data.lat + 'N ' + data.lon+'E';
+
+currentWeatherItemsEl.innerHTML =
+`<div class="weather-item">
+  <div>Humidity</div>
+  <div>${humidity}%</div>
+</div>
+<div class="weather-item">
+  <div>Wind Speed</div>
+  <div>${wind_speed}</div>
+</div>`;
+
+let otherDayForecast = ''
+data.daily.forEach((day, idx) => {
+  if(idx == 0){
+    currentTempEl.innerHTML = `
+    <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+    <div class="others" id="other">
+        <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+        <div class="temp">Night ${day.temp.night}&#176; F</div>
+        <div class="temp">Day ${day.temp.day}&#176; F</div>
+    </div>
+    
+    `
+
+  }else{
+    otherDayForecast += `
+    <div class="weather-forecast-item">
+                <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                <div class="temp">Night - ${day.temp.night}&#176; F</div>
+                <div class="temp">Day - ${day.temp.day}&#176; F</div>
+            </div>`
+  }
+})
+
+  weatherForecastEl.innerHTML = otherDayForecast;
+}
