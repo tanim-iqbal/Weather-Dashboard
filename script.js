923ee401cd099f73d3588926c559fb7f09fec924
Tanim Iqbal <89047977+tanim-iqbal@users.noreply.github.com>
2022-02-09 03:32:50 -0500

    search function working

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const timezone = document.getElementById("time-zone");
 const cityEl = document.getElementById("city");
 const weatherForecastEl = document.getElementById("weather-forecast");
 const currentTempEl = document.getElementById("current-temp");
+const fetchButton = document.getElementById("searchBtn");
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-text");
 
 const days = [
   "Sunday",
@@ -43,13 +46,17 @@ setInterval(() => {
   const ampm = hour >= 12 ? "PM" : "AM";
 
   timeEl.innerHTML =
-    (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ":" + (minutes < 10? '0'+minutes: minutes) + " " + `<span id="am-pm">${ampm}</span>`;
+    (hoursIn12HrFormat < 10 ? "0" + hoursIn12HrFormat : hoursIn12HrFormat) +
+    ":" +
+    (minutes < 10 ? "0" + minutes : minutes) +
+    " " +
+    `<span id="am-pm">${ampm}</span>`;
 
   dateEl.innerHTML = days[day] + ", " + date + " " + months[month];
 }, 1000);
 
-getWeatherData();
-function getWeatherData() {
+getCurrentWeatherData();
+function getCurrentWeatherData() {
   navigator.geolocation.getCurrentPosition((success) => {
     console.log(success);
 
@@ -66,14 +73,53 @@ function getWeatherData() {
   });
 }
 
+function getApi(citySearch) {
+
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+citySearch+'&appid=90a759efbdc05d8004d62a39a8c185c9&units=imperial&'
+  console.log (requestUrl)
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var lat = data.city.coord.lat
+      console.log (lat)
+      var lon = data.city.coord.lon
+      var mainUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=minutely&appid='+API_Key+'&units=imperial&';
+      console.log (lon)
+//---------------------------------------------------------------------------
+  fetch(mainUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (forcastData) {
+
+        showWeatherData(forcastData);
+      })
+    });
+}
+//Search function
+todoForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  console.log("Event Listener Working");
+
+  var citySearch = todoInput.value.trim();
+  // Return from function early if submitted todoText is blank
+  if (citySearch === "") {
+    return;
+  }
+  getApi(citySearch)
+});
+
 function showWeatherData(data) {
   let { humidity, wind_speed } = data.current;
 
   timezone.innerHTML = data.timezone;
-  cityEl.innerHTML = data.lat + 'N ' + data.lon+'E';
+  cityEl.innerHTML = data.lat + "N " + data.lon + "E";
 
-currentWeatherItemsEl.innerHTML =
-`<div class="weather-item">
+  currentWeatherItemsEl.innerHTML = `<div class="weather-item">
   <div>Humidity</div>
   <div>${humidity}%</div>
 </div>
@@ -82,29 +128,46 @@ currentWeatherItemsEl.innerHTML =
   <div>${wind_speed}</div>
 </div>`;
 
-let otherDayForecast = ''
-data.daily.forEach((day, idx) => {
-  if(idx == 0){
-    currentTempEl.innerHTML = `
-    <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+  let otherDayForecast = "";
+  data.daily.forEach((day, idx) => {
+    if (idx == 0) {
+      currentTempEl.innerHTML = `
+    <img src="http://openweathermap.org/img/wn/${
+      day.weather[0].icon
+    }@4x.png" alt="weather icon" class="w-icon">
     <div class="others" id="other">
-        <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
-        <div class="temp">Night ${day.temp.night}&#176; F</div>
+        <div class="day">${window.moment(day.dt * 1000).format("ddd")}</div>
         <div class="temp">Day ${day.temp.day}&#176; F</div>
+        <div class="temp">Night ${day.temp.night}&#176; F</div>
+        
     </div>
     
-    `
-
-  }else{
-    otherDayForecast += `
+    `;
+    } else {
+      otherDayForecast += `
     <div class="weather-forecast-item">
-                <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
-                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-                <div class="temp">Night - ${day.temp.night}&#176; F</div>
+                <div class="day">${window
+                  .moment(day.dt * 1000)
+                  .format("ddd")}</div>
+                <img src="http://openweathermap.org/img/wn/${
+                  day.weather[0].icon
+                }@2x.png" alt="weather icon" class="w-icon">
                 <div class="temp">Day - ${day.temp.day}&#176; F</div>
-            </div>`
-  }
-})
+                <div class="temp">Night - ${day.temp.night}&#176; F</div>
+      </div>`;
+    }
+  });
 
   weatherForecastEl.innerHTML = otherDayForecast;
 }
+
+//search by pressing enter
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var citySearch = todoInput.value.trim();
+  // Return from function early if submitted todoText is blank
+  if (citySearch === "") {
+    return;
+  }
+  getApi(citySearch);
+});
